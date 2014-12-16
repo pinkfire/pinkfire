4875715c38a3b6f4db2b9ca64e743e9eec8890f0
iamluc <luc@vieillescazes.net>
2014-12-16 17:06:50 +0100

    Improve design

--- public/scripts/controllers/list.js
+++ public/scripts/controllers/list.js
@@ -4,6 +4,10 @@ angular.module('sossoaApp')
     .controller('ListCtrl', function($scope, ThreadRepository) {
         $scope.threads = ThreadRepository;
 
+        $scope.toggle = function(id) {
+            $('#thread-'+id).slideToggle();
+        }
+
         var socket = io();
         socket.on('threads', function(thread) {
             ThreadRepository.add(thread);
