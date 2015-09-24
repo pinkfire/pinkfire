153777047427d0c9969d650bd086f6cd1e1452ea
mRoca <mroca.dh@gmail.com>
2015-09-24 14:13:23 +0200

    Add debug option

--- public/scripts/controllers/list.js
+++ public/scripts/controllers/list.js
@@ -7,6 +7,18 @@ angular.module('sossoaApp')
         $rootScope.by_channel = $routeParams.by_channel;
         ThreadRepository.addChannel($rootScope.by_channel);
 
+        $scope.listFilter = function(element) {
+            if ($rootScope.by_channel && element.channel !== $rootScope.by_channel) {
+                return false;
+            }
+
+            if ($rootScope.hide_debug && element.hidden.is_debug) {
+                return false;
+            }
+
+            return true;
+        };
+
         $scope.toggle = function(id) {
             $('#thread-'+id).slideToggle();
         };
